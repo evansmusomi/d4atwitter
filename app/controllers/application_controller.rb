3f69f8493513a8931461df87b5e85a79cd0a369f
Evans Musomi <evanso.dev@gmail.com>
2016-08-20 00:31:14 +0300

    Adds localisation to app. Swahili language included.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  # Sets the locale to use
+  def set_locale
+  	I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
